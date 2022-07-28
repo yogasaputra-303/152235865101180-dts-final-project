8d6b1a933dfa12c8a55d87fcd93bb3f30c0157b9
yoga-saputra <yogsyogs98@gmail.com>
2022-07-28 10:26:15 +0700

    Menambahkan Pencarian

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Pages from "./containers/Pages";
 import Kategori from "./components/Kategori";
 import { BrowserRouter } from 'react-router-dom';
+import Pencarian from "./components/Pencarian";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Pencarian />
         <Kategori />
         <Pages />
       </BrowserRouter>
--- src/components/Kategori.jsx
+++ src/components/Kategori.jsx
@@ -1,27 +1,26 @@
-import {FaHamburger} from 'react-icons/fa';
-import {GiNoodles, GiChopsticks, GiFullPizza} from 'react-icons/gi';
+import {GiNoodles, GiSandwich, GiFullPizza, GiHotDog} from 'react-icons/gi';
 import styled from 'styled-components';
 import{NavLink} from 'react-router-dom';
 
 function Kategori() {
   return (
     <List>
-        <NavLink to={'/cuisine/Italian'}>
+        <Slink to={'/cuisine/Italian'}>
             <GiFullPizza />
             <h4>Italian</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/American'}>
-            <FaHamburger />
-            <h4>American</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Chinese'}>
+        </Slink>
+        <Slink to={'/cuisine/Spanish'}>
+            <GiHotDog />
+            <h4>Spanish</h4>
+        </Slink>
+        <Slink to={'/cuisine/Chinese'}>
             <GiNoodles />
             <h4>Chinese</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Japanese'}>
-            <GiChopsticks />
-            <h4>Japanese</h4>
-        </NavLink>
+        </Slink>
+        <Slink to={'/cuisine/French'}>
+            <GiSandwich />
+            <h4>French</h4>
+        </Slink>
     </List>
   )
 }
@@ -31,5 +30,41 @@ const List = styled.div`
     justify-content: center;
     margin: 2rem 0rem;
 `;
+const Slink = styled(NavLink)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 2rem;
+    text-decoration: none;
+    background: linear-gradient(35deg, #484848, #313131);
+    width: 6rem;
+    height: 6rem;
+    cursor: pointer;
+    transform: scale(0.8);
+    position: relative;
 
-export default Kategori
+    h4{
+        color: white;
+        font-size: 0.8rem;
+    }
+    svg{
+        position: absolute;
+        bottom: 55%;
+        color: white;
+        font-size: 1.5rem;
+    }
+    &.active{
+        background: linear-gradient(to right, #f27121, #e94057);
+        svg{
+            color: white;
+        }
+        h4{
+            color: white;
+        }
+        
+    }
+
+`
+
+export default Kategori;
